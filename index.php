5cd18df604a7d3bc5adfeb8b0c419cda06e92ba5
Isaac <isaac.delauney@etu.unicaen.fr>
2024-11-17 12:55:37 +0100

    :tada: Created the structure of the project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DungeonXPlorer</title>
+    <link rel="stylesheet" href="./style/style.css">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="./script/tailwind.config.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
