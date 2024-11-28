b2ebfaa688f7ccfee68421f0d96dffacbfdd9e50
BEUVE <beuve225@unicaen.fr>
2024-11-28 10:06:27 +0100

    ajout de la page

---
+++ connexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="./style/style.css">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="public/script/tailwind.config.js"></script>
+</head>
+<body>
+    <h1> Connexion <h1> 
+</body>
+</html>
