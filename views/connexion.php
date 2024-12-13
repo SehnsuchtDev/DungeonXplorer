f6f4ff88c1ad77123b9ce892fb9eaf5577ff4538
BEUVE <beuve225@unicaen.fr>
2024-12-13 09:36:05 +0100

    :lipstick: Add connection view

---
+++ views/connexion.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="./style/style.css">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="public/script/tailwind.config.js"></script>
+</head>
+<body>
+    <h1 class="text-center text-4xl"> Connexion </h1> 
+    <div class="place-self-center">
+    <form method="get" action="">
+            <label>Votre adresse mail:</label> <br/>
+            <input type="email" name="mail" placeholder="Veuillez renter votre adresse mail" class="border-2 w-[36vw]">
+            
+        </form>
+    </div>
+        
+    
+    
+
+
+    
+    
+</body>
+</html>
