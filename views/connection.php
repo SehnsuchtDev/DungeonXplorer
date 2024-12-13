526437165db75b52250c2cf00a7af00d976a3aff
BEUVE <beuve225@unicaen.fr>
2024-12-13 10:49:37 +0100

    :art: add to ccs on login view

---
+++ views/connection.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="./style/style.css">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="public/script/tailwind.config.js"></script>
+</head>
+<body class="bg-[#000000] text-[#FFFFFF]">
+    <h1 class="text-center text-4xl px-16 py-16"> Connexion </h1> 
+    <div class="place-self-center  bg-[#582900]">
+        <form method="get" action="">
+            <div class="px-4 py-4 ">
+                <label >Votre adresse mail:</label> 
+                <input type="email" name="mail" placeholder="Veuillez rentrer votre adresse mail" class="border-2 w-[25vw] bg-[#2E2E2E]"> <br/>
+            </div>
+            
+            <div class="px-4 py-4">
+                <label>Votre mot de passe:</label>
+                <input type="email" name="mail" placeholder="Veuillez rentrer votre mot de passe" class="border-2 w-[25vw] bg-[#2E2E2E]">
+            </div>
+        </form>
+    </div>
+        
+    
+    
+
+
+    
+    
+</body>
+</html>
