dd09007eb3e42353760c20483adec2e75e29cb8a
BEUVE <beuve225@unicaen.fr>
2024-12-13 15:42:20 +0100

    :iphone: responsive width

--- views/connection.php
+++ views/connection.php
@@ -13,21 +13,22 @@
     <div class="place-self-center  bg-[#582900]">
         <h1 class="text-center text-4xl p-16"> Connexion </h1> 
         <form method="post" action="">
-            <div class="p-4 ">
-                <label class="px-5">Votre adresse mail:</label> 
+        <div class="flex justify-center items-center p-4 max-[600px]:flex-col">
+                <label class="px-5">Votre adresse mail:</label>
                 <input type="email" name="mail" placeholder="Veuillez rentrer votre adresse mail" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
-            </div>
-            
-            <div class="px-4 py-4">
-                <label class="px-4">Votre mot de passe:</label>
-                <input type="password" name="motDePasse" placeholder="Veuillez rentrer votre mot de passe" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
-            </div>
-        </form>
+        </div> 
 
+        <div class="flex justify-center items-center p-4 max-[600px]:flex-col">
+                <label class="px-5">Votre mot de passe:</label>
+                <input type="password" name="motDePasse" placeholder="Veuillez rentrer votre mot de passe" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
+        </div> 
+
+        </form>
         <div class="flex flex-col items-center space-y-4 p-32">
             <button class=" bg-[#2E2E2E] p-4 w-48  rounded"> Se connecter </button> 
             <p> ou </p> 
-            <button class=" bg-[#2E2E2E] p-4 w-48 rounded" > S'inscrire </button> 
+            <button class=" bg-[#2E2E2E] p-4 w-48 rounded" > S'inscrire
+                 </button> 
         </div>
         
     </div> 
