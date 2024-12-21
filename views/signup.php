9aaed02acd388c13e8206b0cdcf99eb41eaae1d9
BEUVE <beuve225@unicaen.fr>
2024-12-21 11:28:19 +0100

    :lipstick:modify the registration login page

--- views/signup.php
+++ views/signup.php
@@ -11,24 +11,27 @@
 <body class="bg-[#000000] text-[#FFFFFF]">
     
     <div class="place-self-center  bg-[#582900]">
-        <h1 class="text-center text-4xl p-16"> Connexion </h1> 
+        <h1 class="text-center text-4xl p-16"> Inscription </h1> 
         <form method="post" action="">
-        <div class="flex justify-center items-center p-4 max-[600px]:flex-col">
-                <label class="px-5">Votre adresse mail:</label>
-                <input type="email" name="mail" placeholder="Veuillez rentrer votre adresse mail" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
-        </div> 
+            <div class="flex justify-between items-center p-4 max-[600px]:flex-col">
+                    <label class="px-5">Votre pseudo:</label>
+                    <input type="text" name="pseudo" placeholder="Veuillez rentrer pseudo" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
+            </div> 
 
-        <div class="flex justify-center items-center p-4 max-[600px]:flex-col">
-                <label class="px-5">Votre mot de passe:</label>
-                <input type="password" name="motDePasse" placeholder="Veuillez rentrer votre mot de passe" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
-        </div> 
+            <div class="flex justify-between items-center p-4 max-[600px]:flex-col">
+                    <label class="px-5">Votre adresse mail:</label>
+                    <input type="email" name="mail" placeholder="Veuillez rentrer votre adresse mail" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
+            </div>
 
+            <div class="flex justify-between items-center p-4 max-[600px]:flex-col">
+                    <label class="px-5">Votre mot de passe:</label>
+                    <input type="password" name="motDePasse" placeholder="Veuillez rentrer votre mot de passe" class="border-2 w-[30vw] bg-[#2E2E2E] rounded text-center">
+            </div> 
         </form>
         <div class="flex flex-col items-center space-y-4 p-32">
-            <button class=" bg-[#2E2E2E] p-4 w-48  rounded"> Se connecter </button> 
+            <button class=" bg-[#2E2E2E] p-4 w-48  rounded"> S'inscrire </button> 
             <p> ou </p> 
-            <button class=" bg-[#2E2E2E] p-4 w-48 rounded" > S'inscrire
-                 </button> 
+            <button class=" bg-[#2E2E2E] p-4 w-48 rounded" > Se connecter </button> 
         </div>
         
     </div> 
