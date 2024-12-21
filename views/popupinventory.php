c7582d314af1194bbdd57e826fc93e1539c54fd2
BEUVE <beuve225@unicaen.fr>
2024-12-21 22:24:52 +0100

    :construction:display a div with a button

---
+++ views/popupinventory.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pop-up</title>
+   	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="public/script/tailwind.config.js"></script>
+    <script defer src="./script/popupinventory.js"></script>
+</head>
+<body>
+    <button onclick="displayInventory()">Cliquer ici</button>
+    <div id="inventory">
+        <thead>
+            <tr>
+                <th> a </th>
+            </tr>
+        </thead>
+    </div>
+</body>
+
+</html>
---
+++ views/script/popupinventory.js
@@ -0,0 +1,8 @@
+const popup = document.getElementById("inventory");
+popup.style.visibility = "hidden";
+
+function displayInventory(){
+    
+    popup.style.visibility = "visible";
+   
+}
