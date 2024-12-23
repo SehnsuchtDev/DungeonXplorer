c2391242aec238ae7c95ddedd4a614a9b6d3794f
KDual Louane <kdual231@unicaen.fr>
2024-12-24 00:21:35 +0100

    :lipstick: adding choice chapter page

--- views/book.php
+++ views/book.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book test</title>
-	<script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <script type="module" src="../public/script/bookmanager.js"></script>
 </head>
+
 <body>
     <div id="book" class="pointer-events-none z-0">
-        <?php include "./booktest/StoryMode_Load.php"; ?>
+        <?php include "./booktest/ChoiceChapter_Load.php"; ?>
     </div>
 </body>
+
 </html>
