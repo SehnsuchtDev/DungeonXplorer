f98958fd845a74b38b8321da7182bd96a01caf41
BEUVE <beuve225@unicaen.fr>
2024-12-25 23:26:05 +0100

    :lipstick: table and button

---
+++ views/admincontent.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Gestionnaire des comptes </title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="public/script/tailwind.config.js"></script>
+    </head>
+
+    <body>
+    <h1 class="p-10 text-center text-2xl"> Gestionnaire des comptes</h1>
+
+    <table class="w-full border border-gray-300 rounded shadow"> 
+        <tr>
+            <td class="p-5">
+               
+                <p> elt1 </p>
+            </td>
+            
+            <td class="p-4 text-right">
+                <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"> Modifier</button>
+                <button class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 ml-2">Supprimer</button>
+            </td>
+        </tr>
+       
+        <tr>
+            <td class="p-5">
+               
+                <p> elt2 </p>
+            </td>
+            
+            <td class="p-4 text-right">
+                <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"> Modifier</button>
+                <button class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 ml-2">Supprimer</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td class="p-5">
+               
+                <p> elt3 </p>
+            </td>
+            
+            <td class="p-4 text-right">
+                <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-950"> Modifier</button>
+                <button class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-950 ml-2">Supprimer</button>
+            </td>
+        </tr>
+    </table>
+
+    <div class="flex justify-center p-5">
+    <button class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-950 "> Nouveau </button>
+    </div>
+    
+
+
+</body>
+</html>
+
+ 
