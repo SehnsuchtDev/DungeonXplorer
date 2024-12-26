eb2b9c6c3b4fd6aedaa6960e7077afa781f2784f
BEUVE <beuve225@unicaen.fr>
2024-12-26 23:52:25 +0100

    :lipstick: page content

--- views/contentManagementFrame.php
+++ views/contentManagementFrame.php
@@ -1,6 +1,40 @@
 <tr class="border border-[#C4975E]">
     <td class="p-10">
+        <div class="flex">
+            <p> Numéro du chapitre &nbsp;</p>
+            <input type="text" value="Chapitre 6" class="max-h-6 rounded bg-[#2e2e2e]">
+        </div>
+        <div class="flex">
+            <p> Nom du chapitre : &nbsp;</p>
+            <input type="text" value="Le loup noir" class="max-h-6 rounded bg-[#2e2e2e]">
+        </div>
+        <div class="flex">
+            <p>Contenu du chapitre &nbsp;</p>
+            <input type="text" value="À mesure que vous avancez, un bruissement attire votre attention. Une silhouette sombre
+                s’élance soudainement devant vous : un loup noir aux yeux perçants. Son poil est hérissé
+                et sa gueule laisse entrevoir des crocs acérés. Vous sentez son regard fixé sur vous, prêt
+                à bondir." class="max-h-6 rounded bg-[#2e2e2e]">
+        </div>
 
+        <div class="flex flex-col">
+            <p>Choix: &nbsp;</p>
+             <div class="flex flex-col">
+                <div class="flex flex-row">
+                    <input type="text" value="Si vous survivez au loup" class=" rounded bg-[#2e2e2e] ml-10">
+                    <input type="text" value="Chapitre 7" class=" rounded bg-[#2e2e2e] indent-10">
+                </div>
+                
+                <div class="flex flex-row">
+                    <input type="text" value="Si le loup vous terrasse" class=" rounded bg-[#2e2e2e] ml-10">
+                    <input type="text" value="Chapitre 10" class=" rounded bg-[#2e2e2e] mx-10">
+                </div>
+                
+            </div>
+        </div>
+
+            
+        </div>
+        
        
 
     </td>
