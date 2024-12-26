391724e3515fca4836ecb8886dec90f0b61e9168
KDual Louane <kdual231@unicaen.fr>
2024-12-26 11:08:27 +0100

    :lipstick: adding link to index via DungeonXplorer's logo

--- views/shared/header.php
+++ views/shared/header.php
@@ -2,9 +2,11 @@
 <header class="bg-[#2e2e2e] flex flex-row justify-between">
 
     <div class="flex flex-row">
-        <img src="..\public\assets\Logo.png" title="logoDungeonXplorer" class="size-28
-            max-[380px]:size-24
-            max-[380px]:self-center">
+        <a href="../views/index.php">
+            <img src="..\public\assets\Logo.png" title="logoDungeonXplorer" class="size-28
+                max-[380px]:size-24
+                max-[380px]:self-center">
+        </a>
 
         <span class="material-icons my-auto ml-7 text-[#e5e5e5] 
             max-[615px]:invisible
@@ -20,10 +22,10 @@
 
         <div class="bg-[#C4975E] my-7 mx-6 place-content-center rounded-lg 
             max-[615px]:invisible
-            max-[615px]:absolute" >
+            max-[615px]:absolute">
             <p class="font-['Pirata_One'] mx-3 text-[#e5e5e5]">Mode Histoire</p>
         </div>
-        
+
         <div class="bg-[#C4975E] my-7 mx-6 place-content-center rounded-lg 
             max-[615px]:invisible">
             <p class="font-['Pirata_One'] mx-3 text-[#e5e5e5]">Se déconnecter</p>
@@ -36,7 +38,8 @@
             </span>
         </div>
 
-        <div class="bg-[#C4975E] absolute invisible p-3 rounded m-2 mt-24 z-30 max-[615px]:visible"> <!-- max-[615px]:visible -->
+        <div class="bg-[#C4975E] absolute invisible p-3 rounded m-2 mt-24 z-30 max-[615px]:visible">
+            <!-- max-[615px]:visible -->
             <div class="flex flex-row">
                 <span class="material-icons text-[#e5e5e5]">
                     person
