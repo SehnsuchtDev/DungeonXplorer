292db7522efaf03ee469ada39034442049035a54
kuani50 <116524310+kuani50@users.noreply.github.com>
2024-12-26 02:11:31 +0100

    :bug: Fix getter and setter merge error

--- models/hero/Hero.php
+++ models/hero/Hero.php
@@ -195,22 +195,6 @@ abstract class Hero{
         return $this->currentLevel;
     }
 
-    public function getCurrentChapter(){
-        return $this->currentChapter;
-    }
-
-    public function getInitiative(){
-        return $this->initiative;
-    }
-
-    public function getPV(){
-        return $this->pv;
-    }
-
-    public function getStrength(){
-        return $this->strength;
-    }
-
     public function getName(){
         return $this->name;
     }
@@ -227,10 +211,6 @@ abstract class Hero{
         return $this->biography;
     }
 
-    public function getPurse(){
-        return $this->purse;
-    }
-
     public function getPrimaryWeapon(){
         return $this->primaryWeapon;
     }
@@ -239,15 +219,8 @@ abstract class Hero{
         return $this->secondaryWeapon;
     }
 
-    public function getXP(){
-        return $this->xp;
-    }
-    
     public abstract function attack(Monster $monster):void;
-  
-    public function setPV(int $pv){
-        $this->pv = $pv;
-    }
+
 
     //Renvoie 1 si le monstre à l'initiative et 0 si c'est le héros
     public function initiativeCalcul(Monster $monster){
@@ -266,8 +239,3 @@ abstract class Hero{
     }
 
 }
-
-
-
-
-?>
