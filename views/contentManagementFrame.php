d98a5585b8dff917d6ce4145f6e25123f414789d
BEUVE <beuve225@unicaen.fr>
2024-12-27 16:53:40 +0100

    :iphone: end of responsive

--- views/contentManagementFrame.php
+++ views/contentManagementFrame.php
@@ -1,55 +1,40 @@
-<tr class="border border-[#C4975E]">
-    <td>
-    <div class="flex">
-            
-           <img src="../public/assets/Village01.jpg" alt="image chapitre" />
+<tr class="border border-[#C4975E] flex flex-col sm:flex-row">
+    <td class="p-4 sm:p-6">
+        <div>
+            <img src="../public/assets/Village01.jpg" alt="image chapitre" class="w-32 sm:w-48 lg:w-64" />
         </div>
-
     </td>
-    <td class="p-10">
+    
+    <td>
         <div>
             <p class="font-bold"> Numéro du chapitre: &nbsp;</p>
-            <input type="text" value="Chapitre 6" class="max-h-6 rounded bg-[#2e2e2e]">
+            <input type="text" value="Chapitre 6" class="max-h-6 rounded bg-[#2e2e2e] w-full sm:w-auto">
         </div>
         <div>
             <p class="font-bold"> Nom du chapitre : &nbsp;</p>
-            <input type="text" value="Le loup noir" class="max-h-6 rounded bg-[#2e2e2e]">
+            <input type="text" value="Le loup noir" class="max-h-6 rounded bg-[#2e2e2e] w-full sm:w-auto">
         </div>
         <div>
-            <p class="font-bold">Contenu du chapitre: &nbsp;</p>
-            <input type="text" value="À mesure que vous avancez, un bruissement attire votre attention. Une silhouette sombre
-                s’élance soudainement devant vous : un loup noir aux yeux perçants. Son poil est hérissé
-                et sa gueule laisse entrevoir des crocs acérés. Vous sentez son regard fixé sur vous, prêt
-                à bondir." class="max-h-6 rounded bg-[#2e2e2e]">
+            <p class="font-bold"> Contenu du chapitre: &nbsp;</p>
+            <input type="text" value="À mesure que vous avancez, un bruissement attire votre attention..." class="max-h-6 rounded bg-[#2e2e2e] w-full sm:w-auto">
         </div>
 
-    
-            
-     
-        <div>
+        <div class="mt-4">
             <p class="font-bold"> Choix du chapitre: </p>
             <div class="bg-white rounded px-4 py-2 flex justify-between items-center text-black">
                 <span id="chapter">Sélectionnez un chapitre</span>
                 <button onclick="chapterSelection()"> &darr;</button>
             </div>
 
-            <ul class="bg-white rounded" id="pulldownmenu">
+            <ul class="bg-white rounded mt-2 " id="pulldownmenu">
                 <li onclick="selectChapter(this)" class="text-black"> Chapitre 7 </li>
                 <li onclick="selectChapter(this)" class="text-black"> Chapitre 10 </li>
             </ul>
-    </div>
-
-  
-            
-
-
- 
-
-    <td class="p-7 text-right text-lg flex-col">
-        <button class="bg-[#C4975E] text-white w-24 h-10 rounded hover:bg-[#C49700] my-1.5"> Modifier</button>
-        <button class="bg-[#C4975E] text-white w-24 h-10 rounded hover:bg-[#C49700] ml-2 my-1.5">Supprimer</button>
+        </div>
     </td>
 
-
-        
+    <td class="p-4  flex flex-col">
+        <button class="bg-[#C4975E] text-white w-full  rounded hover:bg-[#C49700] my-5"> Modifier</button>
+        <button class="bg-[#C4975E] text-white w-full  rounded hover:bg-[#C49700] ">Supprimer</button>
+    </td>
 </tr>
