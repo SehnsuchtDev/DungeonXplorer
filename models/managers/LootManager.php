3529dea34ee7db4276491cb827a020e958f606cd
kuani50 <116524310+kuani50@users.noreply.github.com>
2024-12-27 15:53:07 +0100

    :sparkles: Spell loot implementation

--- models/managers/LootManager.php
+++ models/managers/LootManager.php
@@ -6,6 +6,7 @@ use dungeonxplorer\item\potion\Effect;
 use dungeonxplorer\loot\gain\GainEffect;
 use dungeonxplorer\loot\gain\GainItem;
 use dungeonxplorer\loot\gain\GainPiece;
+use dungeonxplorer\loot\gain\GainSpell;
 use dungeonxplorer\loot\Loot;
 
 require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'autoload.php';
@@ -41,11 +42,13 @@ class LootManager
                 $gain->it_id = $row['it_id'];
             }elseif(isset($row['lo_effet'],$row['lo_quantity'])){
                 $gain = new GainEffect(Effect::getEffect($row['lo_effet']),$row['lo_quantity']);
-            }elseif(isset($row['lo_piece'])){
+            }elseif(isset($row['lo_piece'])) {
                 $gain = new GainPiece($row['lo_piece']);
+            }elseif(isset($row['lo_spell'])){
+                $spell = SpellManager::getInstance()->getSpell($row['lo_spell']);
+                $gain = new GainSpell($spell);
             }else
                 throw new \InvalidArgumentException('Loot not found');
-
             $loot->addGain($gain);
         }
 
