3ef6531652bbfc4c6bc06df46a30393093b9ebf9
kuani50 <116524310+kuani50@users.noreply.github.com>
2024-12-27 16:10:30 +0100

    :sparkles: Password confirmation on signup page

--- views/signup.php
+++ views/signup.php
@@ -16,13 +16,13 @@
 
         <span class="flex justify-between items-center p-2 flex-col">
             <label class="px-4 text-[2.5vh]">Mot de passe:</label>
-            <input type="password" name="motDePasse" placeholder="Veuillez rentrer votre mot de passe"
+            <input type="password" name="password" placeholder="Veuillez rentrer votre mot de passe"
                 class="text-[2.5vh] border-2 w-[25vw] min-w-64 bg-[#2E2E2E] rounded text-center pointer-events-auto z-0">
         </span>
 
         <span class="flex justify-between items-center p-2 flex-col">
             <label class="px-4 text-[2.5vh]">Confirmer le mot de passe:</label>
-            <input type="password" name="motDePasse" placeholder="Veuillez confirmer votre mot de passe"
+            <input type="password" name="passwordconfirm" placeholder="Veuillez confirmer votre mot de passe"
                 class="text-[2.5vh] border-2 w-[25vw] min-w-64 bg-[#2E2E2E] rounded text-center pointer-events-auto z-0">
         </span>
 
