991555deb298ce8a875bd863220af6c49f4f7426
KDual Louane <kdual231@unicaen.fr>
2024-12-27 16:36:43 +0100

    :iphone: fixing responsive connection and signup

--- views/connection.php
+++ views/connection.php
@@ -8,25 +8,24 @@
         <span class="flex justify-between items-center p-2 flex-col">
             <label class="text-[2.5vh] m-3">E-mail:</label>
             <input id="mail" type="email" name="email" placeholder="Veuillez rentrer votre adresse mail"
-                class="text-[2.5vh] border-2 w-[25vw] min-w-64 bg-[#2E2E2E] rounded text-center pointer-events-auto z-0">
+                class="text-[2.5vh] border-2 w-[25vw] min-w-64 max-w-80 bg-[#2E2E2E] rounded text-center pointer-events-auto z-0">
         </span>
 
         <span class="flex justify-between items-center p-2 flex-col">
             <label class="text-[2.5vh] m-3">Mot de passe:</label>
             <input type="password" name="password" placeholder="Veuillez rentrer votre mot de passe"
-                class=" text-[2.5vh] border-2 w-[25vw] min-w-64 bg-[#2E2E2E] rounded text-center pointer-events-auto z-0">
+                class=" text-[2.5vh] border-2 w-[25vw] min-w-64 max-w-80 bg-[#2E2E2E] rounded text-center pointer-events-auto z-0">
         </span>
 
 
-        <span class="flex flex-col items-center space-y-3 mt-16
-            max-[640px]:mt-10">
-            <input type="submit" value="Se connecter" class=" bg-[#2E2E2E] p-4 w-40 rounded pointer-events-auto
-                max-[640px]:w-32
-                max-[640px]:p-3
-                max-[500px]:p-2" />
-            <p class="text-[3vh] h-1 pb-4"> ou </p>
+        <span class="flex flex-col items-center space-y-3 mt-10">
+            <input type="submit" value="Se connecter" class="text-[2vh] bg-[#2E2E2E] py-4 px-7 rounded pointer-events-auto cursor-pointer
+                    max-[640px]:w-32
+                    max-[640px]:p-3
+                    max-[500px]:p-2" />
+            <p class=""> ou </p>
             <button id="register<?= $seed ?>" type="button"
-                class=" bg-[#2E2E2E] p-2 w-32 text-[3vh] rounded pointer-events-auto"> S'inscrire
+                class=" bg-[#2E2E2E] py-3 px-5 text-[2vh] rounded pointer-events-auto cursor-pointer"> S'inscrire
             </button>
         </span>
 
@@ -37,16 +36,16 @@
 <script defer>
 
 
-        <?php if(isset($errors) && empty($errors)): ?>
-            if(window.bookmanager == undefined){
-                document.addEventListener("DOMContentLoaded",async()=>{
-                    await new Promise(r => setTimeout(r, 300));
-                    window.bookmanager.loadTwoPageAndTurn("<?= FULLURLROOTPATH?>/book/page/hero/p1", "<?= FULLURLROOTPATH?>/book/page/hero/p2");
-                });
-            }
-            else{
-                window.location.reload();
-            }
+    <?php if (isset($errors) && empty($errors)): ?>
+        if (window.bookmanager == undefined) {
+            document.addEventListener("DOMContentLoaded", async () => {
+                await new Promise(r => setTimeout(r, 300));
+                window.bookmanager.loadTwoPageAndTurn("<?= FULLURLROOTPATH ?>/book/page/hero/p1", "<?= FULLURLROOTPATH ?>/book/page/hero/p2");
+            });
+        }
+        else {
+            window.location.reload();
+        }
 
     <?php elseif (isset($errors)): ?>
         str = "";
